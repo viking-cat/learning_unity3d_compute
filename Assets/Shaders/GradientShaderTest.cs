c59503d04b221b5a5ac93714106a36dd713fd2dc
Viking Cat <patrik.kotiranta.lundberg@gmail.com>
2025-03-11 14:40:18 +0100

    Triangle & Gradient Compute Shaders working at the same time

--- Assets/Shaders/GradientShaderTest.cs
+++ Assets/Shaders/GradientShaderTest.cs
@@ -6,14 +6,23 @@ using System.Collections.Generic;
 
 public class GradientShaderTest : MonoBehaviour
 {
-    public ComputeShader computeShader;
-    public RenderTexture renderTexture;
+    public ComputeShader computeTriangleShader;
+    public RenderTexture renderTriangleTexture;
+    
+    public ComputeShader computeGradientShader;
+    public RenderTexture renderGradientTexture;
+
+    public enum ShaderType {
+        TRIANGLE,
+        GRADIENT
+    };
+    public ShaderType shaderType;
 
     private RawImage rawImage;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    void onAwake() {
+    /*void onAwake() {
         //rawImage = gameObject.GetComponent<RawImage>();
         List<RawImage> rawImages = new List<RawImage>(GetComponents<RawImage>());
         Debug.Log("RawImage List Size: " + rawImages.Count);
@@ -24,63 +33,60 @@ public class GradientShaderTest : MonoBehaviour
         } else {
             Debug.Log("onAwake : RawImage was found");
         }
-    }
+    }*/
 
     void Start()
     {
         Debug.Log("Start");
-        
-        renderTexture = new RenderTexture(256, 256, 24);
-        renderTexture.enableRandomWrite = true;
-        renderTexture.Create();
 
-        // "0" is the kernel in the computer shader (exists a find kernal by name function for this)
-        // "Result" is its name in the compute shader
-        computeShader.SetTexture(0, "Result", renderTexture);
-        computeShader.Dispatch(0, renderTexture.width / 8, renderTexture.height / 8, 1);
+        if (shaderType == ShaderType.TRIANGLE) {
+            renderTriangleTexture = new RenderTexture(256, 256, 24);
+            renderTriangleTexture.enableRandomWrite = true;
+            renderTriangleTexture.Create();
+
+            // "0" is the kernel in the computer shader (exists a find kernal by name function for this)
+            // "Result" is its name in the compute shader
+            computeTriangleShader.SetTexture(0, "Result", renderTriangleTexture);
+            computeTriangleShader.Dispatch(0, renderTriangleTexture.width / 8, renderTriangleTexture.height / 8, 1);
+
+            rawImage = gameObject.GetComponent<RawImage>();
+            if (rawImage == null) {
+                Debug.Log("RawImage not found");
+            } else {
+                Debug.Log("RawImage was found");
+                rawImage.texture = (Texture)renderTriangleTexture;
+            }
+
+        } else if (shaderType == ShaderType.GRADIENT) {
+            renderGradientTexture = new RenderTexture(256, 256, 24);
+            renderGradientTexture.enableRandomWrite = true;
+            renderGradientTexture.Create();
+
+            computeGradientShader.SetTexture(0, "Result", renderGradientTexture);
+            computeGradientShader.SetFloat("Resolution", renderGradientTexture.width);
+            computeGradientShader.Dispatch(0, renderGradientTexture.width / 8, renderGradientTexture.height / 8, 1);
+
+            rawImage = gameObject.GetComponent<RawImage>();
+            if (rawImage == null){
+                Debug.Log("RawImage not found");
+            } else {
+                Debug.Log("RawImage was found");
+                rawImage.texture = (Texture)renderGradientTexture;
+            }
+        }
+
+        
+
+        
+
+        
+
+        
 
         // RawImage rawImage = gameObject.GetComponent<RawImage>();
-        rawImage = gameObject.GetComponent<RawImage>();
-        if (rawImage == null) {
-            Debug.Log("RawImage not found");
-        } else {
-            Debug.Log("RawImage was found");
-            rawImage.texture = (Texture)renderTexture;
-        }
+        
         
         // rawImage.mainTexture = renderTexture;
         // rawImage.mainTexture = renderTexture;
     }
-
-    /*private void OnGUI()
-    {
-        rawImage = gameObject.GetComponent<RawImage>();
-        if (rawImage == null) {
-            Debug.Log("OnGUI : RawImage not found");
-        } else {
-            Debug.Log("OnGUI : RawImage was found");
-        }
-
-    }*/
-
-    // Event function that Unity calls after a Camera has finished rendering, that allows you to modify the Camera's final image.
-    // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/MonoBehaviour.OnRenderImage.html
-    /*private void OnRenderImage(RenderTexture src, RenderTexture dest) 
-    {
-        Debug.Log("OnRenderImage");
-        
-        if (renderTexture == null) {
-            renderTexture = new RenderTexture(256, 256, 24);
-            renderTexture.enableRandomWrite = true;
-            renderTexture.Create();
-        }
-
-        computeShader.SetTexture(0, "Result", renderTexture);
-        computeShader.Dispatch(0, renderTexture.width / 8, renderTexture.height / 8, 1);
-
-        Graphics.Blit(renderTexture, dest);
-    }*/
-
-    // Update is called once per frame
-    // void Update(){}
 }
